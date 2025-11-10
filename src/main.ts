ea9298cf0b0f6514c5fbc128800d1ab6dc0962fe
Walter Santos Filho <wcsantosfilho@gmail.com>
2025-11-10 19:21:46 -0300

    tracking deploy on vercel

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  console.log(`Running certo-plata-backend on Vercel: ${process.env.NODE_ENV}`);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
