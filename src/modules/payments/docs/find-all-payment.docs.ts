c7fb3c7cf38ff0640d42cb93032d41b56a945a9a
Walter Santos Filho <wcsantosfilho@gmail.com>
2025-12-24 10:12:10 -0300

    fixing relative paths

--- src/modules/payments/docs/find-all-payment.docs.ts
+++ src/modules/payments/docs/find-all-payment.docs.ts
@@ -1,7 +1,7 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 import { ApiCommonErrors } from '../../../common/decorators/api/api-error.doc';
-import { ApiPaginationQueries } from 'src/common/decorators/api/api-pagination.doc';
+import { ApiPaginationQueries } from '../../../common/decorators/api/api-pagination.doc';
 
 export function ApiFindAllPaymentsDocs() {
   return applyDecorators(
--- src/modules/payments/docs/find-by-associate-payment.doc.ts
+++ src/modules/payments/docs/find-by-associate-payment.doc.ts
@@ -1,7 +1,7 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiQuery, ApiParam } from '@nestjs/swagger';
 import { ApiCommonErrors } from '../../../common/decorators/api/api-error.doc';
-import { ApiPaginationQueries } from 'src/common/decorators/api/api-pagination.doc';
+import { ApiPaginationQueries } from '../../../common/decorators/api/api-pagination.doc';
 
 export function ApiFindByAssociateDocs() {
   return applyDecorators(
