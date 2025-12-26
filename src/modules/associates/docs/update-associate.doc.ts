807015be089fa7347ce83cb5398797f022c6026e
Walter Santos Filho <wcsantosfilho@gmail.com>
2025-12-25 22:17:11 -0300

    fix swagger ApiParam for update associate

--- src/modules/associates/docs/update-associate.doc.ts
+++ src/modules/associates/docs/update-associate.doc.ts
@@ -1,11 +1,18 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { ApiCommonErrors } from '../../../common/decorators/api/api-error.doc';
 
 export function ApiUpdateAssociateDoc() {
   return applyDecorators(
     ApiOperation({
       summary: 'Update an existing associate',
+    }),
+    ApiParam({
+      name: 'id',
+      description: 'ID do associado (UUID)',
+      example: '123e4567-e89b-12d3-a456-426614174000',
+      required: true,
+      type: String,
     }),
     ApiResponse({
       status: 200,
