7ea77ab162afe5225aad5694b02fbe720ab5a1d3
Walter Santos Filho <wcsantosfilho@gmail.com>
2026-02-24 06:28:58 -0300

    add APIParams to orgnization find by id

--- src/modules/organizations/docs/find-by-id-organization.doc.ts
+++ src/modules/organizations/docs/find-by-id-organization.doc.ts
@@ -1,10 +1,18 @@
 import { applyDecorators } from '@nestjs/common';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
 import { ApiCommonErrors } from '../../../common/decorators/api/api-error.doc';
 
 export function ApiFindByIdOrganizationDocs() {
   return applyDecorators(
     ApiOperation({ summary: 'Get an organization by ID' }),
+    ApiParam({
+      name: 'id',
+      description: 'ID da organização (UUID)',
+      example: '123e4567-e89b-12d3-a456-42661412432a',
+      required: true,
+      type: String,
+    }),
+
     ApiResponse({
       status: 200,
       description: 'The organization has been successfully retrieved.',
